76966fde5e1189f9a8b03adb9a56e8a662fe7bf7
Weilin1992 <wz196@rutgers.edu>
2016-05-01 22:40:03 -0400

    finished

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+	name = str(request.GET.get('name'))
+	if name == 'facebook':
+		return HttpResponse('FB://profile')
+	elif name == 'uber':
+		return HttpResponse('Uber://')
+
+
